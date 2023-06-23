11413c53eb051e93928e8e1b1633d36332f9f699
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2023-06-22 23:57:25 -0300

    changes on animation and first commits of website added

---
+++ website.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector('#hamburger-icon');
+let svg = menu.querySelector('svg');
+
+console.log(svg)
+
+menu.addEventListener('click', () => {
+    svg.classList.toggle('open');
+})
+
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentState = button.getAttribute("data-state");
+
+    if (!currentState || currentState === "closed") {
+      button.setAttribute("data-state", "opened");
+      button.setAttribute("aria-expanded", "true");
+    } else {
+      button.setAttribute("data-state", "closed");
+      button.setAttribute("aria-expanded", "false");
+    }
+  });
+});
